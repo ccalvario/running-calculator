891deea45b499a22c8ef88ad540488f5cfd318cb
ccalvario <ccalvariozavala@gmail.com>
2018-11-09 14:10:40 -0800

    Updates

---
+++ precache-manifest.96b8985e269cc352ff0a517cf751fa8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c9927197e8290abfd53",
+    "url": "/running-calculator/static/js/runtime~main.1c992719.js"
+  },
+  {
+    "revision": "fbdee13a96f0520aba18",
+    "url": "/running-calculator/static/js/main.fbdee13a.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/running-calculator/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "fbdee13a96f0520aba18",
+    "url": "/running-calculator/static/css/main.faf894ca.chunk.css"
+  },
+  {
+    "revision": "9c3d0b12d3a42d9353d18517b2373753",
+    "url": "/running-calculator/index.html"
+  }
+];
