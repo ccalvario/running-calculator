3fc2e32cf89373f597f4401116d1f64c3931fd07
ccalvario <ccalvariozavala@gmail.com>
2018-11-12 14:42:01 -0800

    Add header.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Board />
-        </header>
+        <Header />
+        <Board />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => (
+    <nav className="header">
+        <div className="title">Running Calculator</div>
+    </nav>
+);
+
+export default Header;
